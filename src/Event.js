039de76f19bc98a8dcfc743c6f8daaf903672cba
Daniel Zlotin <danielzl@wix.com>
2016-09-13 17:22:33 +0300

    added tests for Event

--- src/Event.js
+++ src/Event.js
@@ -1,6 +1,4 @@
-export function createEvent(params) {
-  // TODO create an event yourself and this will return the obj for redux
-}
+// TODO create an event yourself and this will return the obj for redux
 
 export class Event {
   /**
@@ -33,40 +31,4 @@ export class Event {
   newState(oldState, params) {
     return oldState;
   }
-
-  ///**
-  // * Helper method to override (instead of newState),
-  // * return only the delta you would like to be merged with the old state,
-  // * I will merge it for you.
-  // *
-  // * @param oldState
-  // * @param params - the params you created the event with
-  // * @return a delta to merge the state with
-  // */
-  //merge(oldState, params) {
-  //  return undefined;
-  //}
-  //
-  ///**
-  // * Helper method to override (instead of newState),
-  // * return only the path you would like to be removed from the old state,
-  // * I will create a new state for you with this path removed.
-  // *
-  // * @param oldState
-  // * @param params
-  // * @return {String} path to remove from the state
-  // */
-  //remove(oldState, params) {
-  //  return undefined;
-  //}
 }
-
-//function removeFromOldState(oldState, removePath) {
-//  const clone = _.clone(oldState);
-//  _.unset(clone, removePath);
-//  return clone;
-//}
-//
-//function mergeToOldState(oldState, mergeDelta) {
-//  return _.merge({}, oldState, mergeDelta);
-//}
--- test/Event.spec.js
+++ test/Event.spec.js
@@ -1,1 +1,37 @@
+import 'jasmine-expect';
+import _ from 'lodash';
+import {Event} from '../src/Event';
 
+describe('Event', () => {
+  it('creates a redux action', () => {
+    const params = {};
+    const result = Event.create(params);
+
+    expect(_.isPlainObject(result)).toBeTrue();
+    expect(result.type).toEqual('Event');
+    expect(result._instance).toBeObject();
+    expect(result._instance instanceof Event).toBeTrue();
+  });
+
+  it('prefixes namespace if defined', () => {
+    class SpecificEvent extends Event {
+      static namespace = 'some.name.space.'
+    }
+
+    const params = {};
+    const result = SpecificEvent.create(params);
+
+    expect(_.isPlainObject(result)).toBeTrue();
+    expect(result.type).toEqual('some.name.space.SpecificEvent');
+    expect(result._instance).toBeObject();
+    expect(result._instance instanceof Event).toBeTrue();
+  });
+
+  describe('newState (called by the Reducer)', () => {
+    it('returns oldState by default', () => {
+      const oldState = {};
+      const action = Event.create('');
+      expect(action._instance.newState(oldState)).toBe(oldState);
+    });
+  });
+});
